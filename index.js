73ab22955a9e10b81cf8ce173cc3b67c19af06a0
20060911 <burakkasuga@gmail.com>
2025-09-26 23:56:00 +0300

    Create index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+var app = require("express")();
+var mineflayer = require("mineflayer");
+app.get("/", function(request, response) {
+  mineflayer.createBot({
+    host: request.query.host,
+    port: request.query.port,
+    username: request.query.username
+  });
+  response.send("OK");  
+});
+app.listen(3000);
